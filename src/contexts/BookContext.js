b2717299d0de9529bfaaa561dc2630acea8cb147
Brisa-Codes <brisamukunde1@gmail.com>
2022-12-25 11:58:13 +0300

    reducers structure

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,37 +1,13 @@
-import React, {createContext, useState} from 'react';
-import {v4} from 'uuid'
+import React, {createContext, useReducer} from 'react';
+import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {
-            id: 1,
-            author: 'Saint Brisa',
-            title: 'Book 1',
-        },
-        {
-            id: 2,
-            author: 'Laurent Don',
-            title: 'Book 2',
-        },
-        {
-            id: 1,
-            author: 'The Voice',
-            title: 'Book 3',
-        },
-    ]);
-
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: v4() }])
-    }
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    };
+    const [books, dispatch] = useReducer(BookReducer, []); 
 
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             { props.children }
         </BookContext.Provider>    
     )
